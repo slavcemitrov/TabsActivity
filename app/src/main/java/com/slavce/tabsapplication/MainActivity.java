92e4c5cde83d1ef927f73610b1da2203bfe57fc0
Славче Митров <slavcemitrov92@gmail.com>
2021-12-10 20:32:20 +0100

    Test commit
    Test coomit

--- app/src/main/java/com/slavce/tabsapplication/MainActivity.java
+++ app/src/main/java/com/slavce/tabsapplication/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
 
-
+        //Test cooment
     }
 
 }
